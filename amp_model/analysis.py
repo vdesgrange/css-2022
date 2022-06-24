ad67785b5335c4706f72f5d45281861cc1bb8e0d
Viviane Desgrange <viviane.desgrange@gmail.com>
2022-06-24 13:58:27 +0200

    Work in progress.

---
+++ amp_model/analysis.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def time_analysis(timeline):
+    healthy=[]
+    exposed=[]
+    infected=[]
+    recovered=[]
+    dead=[]
+
+    for i in range(len(timeline)):
+        G = timeline[i]
+        temp = [G.nodes[id]['data'].state for id in list(G.nodes)]
+        healthy.append(temp.count(0))
+        exposed.append(temp.count(1))
+        infected.append(temp.count(2))
+        recovered.append(temp.count(3))
+        dead.append(temp.count(4))
+
+    x = np.linspace(0, len(timeline), len(timeline))
+    plt.plot(x, healthy, 'o', color='blue')
+    plt.plot(x, exposed, 'o', color='yellow')
+    plt.plot(x, infected, 'o', color='red')
+    plt.plot(x, recovered, 'o', color='green')
+    plt.plot(x, dead, 'o', color='black')
+    plt.show()
+
+    return(healthy,exposed,recovered,infected,dead)
--- amp_model/main.py
+++ amp_model/main.py
@@ -2,17 +2,17 @@ import numpy as np
 from networks import Barabasi_Albert_Network
 from graphic_utils import visualize, show_state
 from model import amp_model
+from analysis import time_analysis
 
 def experiment_1():
     """
     Different alpha for small barabasi-albert graph
     :return:
     """
-    bara = Barabasi_Albert_Network(100, 5, 0.01)
-    visualize(bara.G)
-    bara.set_edges('flow', 1)
+    bara = Barabasi_Albert_Network(50, 5, 0.02)
     s = amp_model(bara, 50, [0])
     show_state(s, 'example.gif')
+    time_analysis(s)
 
 
 if __name__ == '__main__':
